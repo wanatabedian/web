4d67d42385a999dcadcfffa0c4b0cef802cdd9a7
wanatabedian <dianindriani21220@gmail.com>
2026-02-08 22:51:49 +0700

    Update perbaikan

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Mengambil elemen ikon menu dan daftar navigasi
+
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
-// Menambahkan fungsi klik pada ikon menu
+
 menuIcon.onclick = () => {
-    // Menambah atau menghapus class 'active' pada nav-links
+   
     navLinks.classList.toggle('active');
 };
 
-// Opsional: Menutup menu secara otomatis saat salah satu link diklik
+
 const allNavLinks = document.querySelectorAll('.nav-links li a');
 
 allNavLinks.forEach(link => {
